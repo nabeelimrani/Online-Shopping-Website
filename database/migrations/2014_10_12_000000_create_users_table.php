e0e0480550846ece4b9da5556fdefb8440b2b5bc
Nabeel Mehdi Imrani <118731608+nabeelimrani@users.noreply.github.com>
2023-06-26 13:44:50 +0500

    testing

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
+
+
+
             
             $table->string('fname');
             $table->string('lname');
